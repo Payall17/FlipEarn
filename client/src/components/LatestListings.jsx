19a026e0a87c7c154dbaca60b14d984cb49b6db5
Payal Palini <mail2payalp@gmail.com>
2026-08-16 22:16:40 +0530

    Update homepage and listing components

---
+++ client/src/components/LatestListings.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Title from './Title'
+
+const LatestListings = () => {
+    const { listings } = useSelector((state) => state.listing)
+
+    return (
+        <div className='mt-20 mb-8'>
+
+            <Title
+                title="Latest Listings"
+                description="Discover the hottest social profiles available right now."
+            />
+
+            <div className='flex flex-col gap-6 px-6'>
+
+                {listings.slice(0, 4).map((listing, index) => (
+                    <div
+                        key={index}
+                        className='mx-auto w-full max-w-4xl border border-gray-300 rounded-lg p-4 flex flex-col md:flex-row gap-4 items-center'
+                    >
+                        <h1>{listing.title}</h1>
+
+                        <h1>{listing.username}</h1>
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default LatestListings
