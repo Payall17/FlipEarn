f8c6bde6d819ca4bd61b64e813b204bf707a41b3
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2026-08-27 00:13:43 +0530

    Remove duplicate src

--- src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import listingSlice from "./features/listingSlice";
-
-export const store = configureStore({
-    reducer: {
-        listing: listingSlice
-    }
-});
