469e45b5fb9da3c787d4ecde63d70d4b6ac7a5e5
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2026-08-27 09:51:02 +0530

    listing card update. clerk authentication plans remain

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { platformIcons } from '../assets/assets'
-import { BadgeCheck } from 'lucide-react'
+import { BadgeCheck, LineChart, MapIcon, MapPin, User } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 
 const ListingCard = ({ listing }) => {
     const currency = import.meta.env.VITE_CURRENCY || '$'
+    const navigate = useNavigate()
 
     return (
         <div className='relative bg-white rounded-2xl shadow-md p-4 flex flex-col gap-2 hover:shadow-lg transition'>
@@ -32,7 +34,53 @@ const ListingCard = ({ listing }) => {
                     </div>
                     {listing.verified && <BadgeCheck className='text-green-500 ml-auto w-5 h-5'/>} 
                 </div>
+                {/* Stats  */}
+                <div className='flex flex-wrap justify-between max-w-lg items-center gap-3 my-5'>
+                    <div className='flex items-center text-sm text-gray-600'>
+                        <User className='size-6 mr-1 text-gray-400'/>
+                        <span className='text-lg font-medium text-slate-800 mr-1.5 '>{listing.followers_count.toLocaleString()}</span> followers
+                    </div>
+                    {listing.engagement_rate && (
+                        <div>
+                            <LineChart className='size-6 mr-1 text-gray-400'/>
+                            <span className='text-lg font-medium text-slate-800 mr-1.5'>
+                                {listing.engagement_rate}
+                            </span> % engagement
+                        </div>
+                    )}
 
+                </div>
+                {/* Tags and Location  */}
+                <div className='flex items-center gap-3 mb-3'>
+                    <span className='text-xs font-medium bg-pink-100 text-pink-600 px-3 py-1 rounded-full capitalize'>{listing.niche}</span>
+                    {listing.country && (
+                        <div className='flex items-center text-gray-500 text-sm'>
+                            <MapPin className='size-6 mr-1 text-gray-400'/>
+                            {listing.country}
+                        </div>
+                    )}
+                </div>
+                {/* Description  */}
+                    <p className='text-sm text-gray-600 mb-4 line-clamp-2'>
+                        {listing.description}
+                    </p>
+                    <hr className='my-5 border-gray-200 '/>
+
+                    {/* Footer  */}
+                    <div className='flex items-center justify-between'>
+                        <div className='flex items-baseline'>
+                            <span className='text-2xl font-medium text-slate-800'>
+                                {currency}
+                                {listing.price.toLocaleString()}
+                            </span>
+                        </div>
+                        <button onClick={()=>{
+                            navigate(`/listing/${listing.id}`);
+                            scrollTo(0,0)
+                        }} className='px-7 py-3 bg-indigo-600 text-white text-sm rounded-lg hover:bg-indigo-700 transition'>
+                            More Details
+                        </button>
+                    </div>
             </div>
 
         </div>
